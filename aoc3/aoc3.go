41a737df7152a20dbb049260b04d009cd850237d
Boris <couturier.boris@gmail.com>
2021-12-08 13:04:19 +0100

    ✨Jour 3 ex2

--- aoc3/aoc3.go
+++ aoc3/aoc3.go
@@ -40,3 +40,72 @@ func Ex1() {
 
 	fmt.Println("total : ", gammaRateInt*epsilonRateInt)
 }
+
+func Ex2() {
+	input := utils.ReadFileSplitToArray("aoc3/aoc3.txt")
+
+	oxygenGenerator := findOxygen(input, 0)
+	co2Scrubber := findco2Scrubber(input, 0)
+
+	fmt.Println(oxygenGenerator)
+	fmt.Println(co2Scrubber)
+
+	oxygenGeneratorInt, _ := strconv.ParseInt(oxygenGenerator, 2, 32)
+	co2ScrubberInt, _ := strconv.ParseInt(co2Scrubber, 2, 32)
+
+	fmt.Println("total : ", oxygenGeneratorInt*co2ScrubberInt)
+}
+
+func findOxygen(input []string, charPosition int) string {
+	var arrayWith1 []string
+	var arrayWith0 []string
+
+	for _, inputValue := range input {
+		if inputValue[charPosition:charPosition+1] == "1" {
+			arrayWith1 = append(arrayWith1, inputValue)
+		} else {
+			arrayWith0 = append(arrayWith0, inputValue)
+		}
+	}
+
+	if charPosition >= len(input[0])-1 {
+		if len(arrayWith1) > 0 {
+			return arrayWith1[0]
+		} else {
+			return arrayWith0[0]
+		}
+	}
+
+	if len(arrayWith1) >= len(arrayWith0) {
+		return findOxygen(arrayWith1, charPosition+1)
+	} else {
+		return findOxygen(arrayWith0, charPosition+1)
+	}
+}
+
+func findco2Scrubber(input []string, charPosition int) string {
+	var arrayWith1 []string
+	var arrayWith0 []string
+
+	for _, inputValue := range input {
+		if inputValue[charPosition:charPosition+1] == "1" {
+			arrayWith1 = append(arrayWith1, inputValue)
+		} else {
+			arrayWith0 = append(arrayWith0, inputValue)
+		}
+	}
+
+	if charPosition >= len(input[0])-1 {
+		if len(arrayWith0) > 0 {
+			return arrayWith0[0]
+		} else {
+			return arrayWith1[0]
+		}
+	}
+
+	if len(arrayWith1) == 0 || (len(arrayWith0) > 0 && len(arrayWith0) <= len(arrayWith1)) {
+		return findco2Scrubber(arrayWith0, charPosition+1)
+	} else {
+		return findco2Scrubber(arrayWith1, charPosition+1)
+	}
+}
--- main.go
+++ main.go
@@ -1,16 +1,15 @@
 package main
 
-import (
-	"adventofcode2021/aoc7"
-)
+import "adventofcode2021/aoc3"
 
 func main() {
-	aoc7.Ex2()
+	// aoc7.Ex2()
 	// aoc7.Ex1()
 	// aoc6.Ex1()
 	// aoc6.Ex2()
 	// aoc5.Ex1()
 	// aoc4.Ex1Ex2()
+	aoc3.Ex2()
 	// aoc3.Ex1()
 	// aoc2.Ex1()
 	// aoc2.Ex2()
